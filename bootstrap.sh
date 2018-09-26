4953c2826c624e05651cbd53ec48d69d29cdd440
Vinod Kumar Nerella <nerellavinod@gmail.com>
2018-09-26 14:57:34 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,14 @@
 
 #Adding the user to the cluter
 #useradd  -mG wheel puppet
-#echo itversity | passwd puppet --stdin
+#echo puppet | passwd puppet --stdin
 sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 service sshd restart
 
 #Uncommenting the sudoers file
 sudo sed -i '/NOPASSWD/s/^#//g' /etc/sudoers
 
-#Copying required files
-#sudo cp /vagrant/main.sh /opt/main.sh 
-sudo cp /vagrant/hadoop-2.9.1.tar.gz /tmp/hadoop-2.9.1.tar.gz
+#Installing wget package
 sudo yum -y install wget
 
 
