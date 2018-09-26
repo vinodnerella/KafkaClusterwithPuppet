52e48da821354ffc5af37b2725d4112a3ce89ab8
Vinod Kumar Nerella <nerellavinod@gmail.com>
2018-09-26 15:02:33 +0530

    Update nodespecific.pp

--- code/environments/production/manifests/nodespecific.pp
+++ code/environments/production/manifests/nodespecific.pp
@@ -46,13 +46,5 @@ file_line { 'Change server.properties file':
   line => 'broker.id=3',
   match   => '^broker.id=',
 }
-class { 'java' :
-  package => 'java-1.8.0-openjdk-devel',
-  }
-
-file { "/etc/profile.d/java_home.sh":
-      content => 'export JAVA_HOME="/usr/lib/jvm/jre-1.8.0-openjdk-1.8.0.181-3.b13.el7_5.x86_64"'
-   }
-
 }
 
